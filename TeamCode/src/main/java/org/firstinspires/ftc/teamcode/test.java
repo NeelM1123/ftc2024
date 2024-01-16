67dd978314664340378615f8cbde4c9628ff194b
shashi <shashi@xdev.lan>
2024-01-16 19:12:20 +0000

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -36,7 +36,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-@TeleOp(name="Linear OpMode", group="Linear OpMode")
+@TeleOp(name="Linear OpMode Test", group="Linear OpMode")
 public class test extends LinearOpMode {
 
     // Declare OpMode members for each of the 2 motors.
@@ -48,10 +48,19 @@ public class test extends LinearOpMode {
     private Servo servo1 = null;
     private Servo servo2 = null;
 
+    double leftPower;
+    double rightPower;
 
+    // run until the end of the match (driver presses STOP)
+    double power1=1.0;
+    double power2=1.0;
+    double damp=0.7;
+    double ratio=0.9;
 
+    double finalPower4 = power1;
+    double finalPower5 = power2;
 
-
+    boolean isConfigMode=false;
 
     @Override
     public void runOpMode() {
@@ -79,58 +88,102 @@ public class test extends LinearOpMode {
 
         waitForStart();
         runtime.reset();
-        double leftPower;
-        double rightPower;
 
-        // run until the end of the match (driver presses STOP)
-        double power1=1.0;
-        double power2=1.0;
-        double damp=0.7;
-        double ratio=0.9;
         //power2=power1*0.6;
         power1=power1*damp;
         power2=power2*damp;
 
 
+
         servo.setPosition(0);
         while (opModeIsActive()) {
-
-            double finalPower4 = power1;
-            double finalPower5 = power2;
-            class CombinedArmsUp {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.left_bumper) {
-                        motor1.setDirection(DcMotor.Direction.FORWARD);
-                        motor2.setDirection(DcMotor.Direction.REVERSE);
-                        motor1.setPower(finalPower4);
-                        motor2.setPower(-finalPower5 * ratio);
-                        sleep(10);
-                        motor1.setPower(0);
-                        motor2.setPower(0);
-                        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
-                }
+            if (gamepad1.back){
+                isConfigMode=!isConfigMode;
+                //telemetry.addLine("ConfigMode: " + isConfigMode);
+                telemetry.addData("Status", "ConfigMode: " + isConfigMode);
+                telemetry.update();
+            }
+            if (isConfigMode){
+                adjustConfig();
+            }else{
+                controlRobot();
             }
 
-            // Combined Arms Down
-            double finalPower6 = power1;
-            double finalPower7 = power2;
-            class CombinedArmsDown {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.right_bumper) {
-                        motor1.setDirection(DcMotor.Direction.FORWARD);
-                        motor2.setDirection(DcMotor.Direction.REVERSE);
-                        motor1.setPower(-finalPower6);
-                        motor2.setPower(finalPower7 * ratio);
-                        sleep(10);
-                        motor1.setPower(0);
-                        motor2.setPower(0);
-                        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }}
+    public void adjustConfig(){
+        boolean updated=false;
+        if (gamepad1.dpad_up){
+            damp+=0.1;
+            if (damp>1)
+                damp=1.0;
+            updated=true;
+        } else if (gamepad1.dpad_down) {
+            damp-=0.1;
+            if (damp<0.2)
+                damp=0.2;
+            updated=true;
+        } else if (gamepad1.dpad_left) {
+            ratio-=0.1;
+            if (ratio<0.1)
+                ratio=0.1;
+            updated=true;
+        } else if (gamepad1.dpad_right) {
+            ratio+=0.1;
+            if (ratio>2)
+                ratio=2;
+            updated=true;
+        }
+        if (updated){
+            telemetry.addData("Status","Config:" + "Damp: " + damp + " Ratio: " + ratio);
+            telemetry.update();
+        }
+
+    }
+    public void controlRobot(){
+
+    }
+
+    public void xd(){
+
+        class CombinedArmsUp {
+            void execute() {
+                while (opModeIsActive() && gamepad1.left_bumper) {
+                    motor1.setDirection(DcMotor.Direction.FORWARD);
+                    motor2.setDirection(DcMotor.Direction.REVERSE);
+                    motor1.setPower(finalPower4);
+                    motor2.setPower(-finalPower5 * ratio);
+                    sleep(10);
+                    motor1.setPower(0);
+                    motor2.setPower(0);
+                    motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 }
             }
+        }
+
+        // Combined Arms Down
+        double finalPower6 = power1;
+        double finalPower7 = power2;
+        class CombinedArmsDown {
+            void execute() {
+                while (opModeIsActive() && gamepad1.right_bumper) {
+                    motor1.setDirection(DcMotor.Direction.FORWARD);
+                    motor2.setDirection(DcMotor.Direction.REVERSE);
+                    motor1.setPower(-finalPower6);
+                    motor2.setPower(finalPower7 * ratio);
+                    sleep(10);
+                    motor1.setPower(0);
+                    motor2.setPower(0);
+                    motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                }
+            }
+        }
 
             /*while (gamepad1.left_bumper == true) {
                 motor1.setDirection(DcMotor.Direction.FORWARD);
@@ -156,72 +209,72 @@ public class test extends LinearOpMode {
                 motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
             }*/
-    //        while (gamepad1.x) {
-    //            motor1.setDirection(DcMotor.Direction.FORWARD);
-    //            motor1.setPower(power1);
-    //            sleep(10);
-    //            motor1.setPower(0);
-    //            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    //        }
+        //        while (gamepad1.x) {
+        //            motor1.setDirection(DcMotor.Direction.FORWARD);
+        //            motor1.setPower(power1);
+        //            sleep(10);
+        //            motor1.setPower(0);
+        //            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //        }
 
-    //        while (gamepad1.y) {
-    //            motor1.setDirection(DcMotor.Direction.FORWARD);
-    //            motor1.setPower(-power1);
-    //            sleep(10);
-    //            motor1.setPower(0);
-    //            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    //        }
-            double finalPower1 = power1;
-            class Arm1Forward {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.left_trigger > 0.5) {
-                        motor1.setDirection(DcMotor.Direction.FORWARD);
-                        motor1.setPower(finalPower1);
-                        sleep(10);
-                        motor1.setPower(0);
-                        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
+        //        while (gamepad1.y) {
+        //            motor1.setDirection(DcMotor.Direction.FORWARD);
+        //            motor1.setPower(-power1);
+        //            sleep(10);
+        //            motor1.setPower(0);
+        //            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //        }
+        double finalPower1 = power1;
+        class Arm1Forward {
+            void execute() {
+                while (opModeIsActive() && gamepad1.left_trigger > 0.5) {
+                    motor1.setDirection(DcMotor.Direction.FORWARD);
+                    motor1.setPower(finalPower1);
+                    sleep(10);
+                    motor1.setPower(0);
+                    motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 }
             }
+        }
 
-            double finalPower = power1;
-            class Arm1Backward {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.right_trigger > 0.5) {
-                        motor1.setDirection(DcMotor.Direction.FORWARD);
-                        motor1.setPower(-finalPower);
-                        sleep(10);
-                        motor1.setPower(0);
-                        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
+        double finalPower = power1;
+        class Arm1Backward {
+            void execute() {
+                while (opModeIsActive() && gamepad1.right_trigger > 0.5) {
+                    motor1.setDirection(DcMotor.Direction.FORWARD);
+                    motor1.setPower(-finalPower);
+                    sleep(10);
+                    motor1.setPower(0);
+                    motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 }
             }
+        }
 
-            double finalPower2 = power2;
-            class Arm2Forward {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.dpad_left) {
-                        motor2.setDirection(DcMotor.Direction.FORWARD);
-                        motor2.setPower(finalPower2);
-                        sleep(10);
-                        motor2.setPower(0);
-                        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
+        double finalPower2 = power2;
+        class Arm2Forward {
+            void execute() {
+                while (opModeIsActive() && gamepad1.dpad_left) {
+                    motor2.setDirection(DcMotor.Direction.FORWARD);
+                    motor2.setPower(finalPower2);
+                    sleep(10);
+                    motor2.setPower(0);
+                    motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 }
             }
+        }
 
-            double finalPower3 = power2;
-            class Arm2Backward {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.dpad_right) {
-                        motor2.setDirection(DcMotor.Direction.FORWARD);
-                        motor2.setPower(-finalPower3);
-                        sleep(10);
-                        motor2.setPower(0);
-                        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    }
+        double finalPower3 = power2;
+        class Arm2Backward {
+            void execute() {
+                while (opModeIsActive() && gamepad1.dpad_right) {
+                    motor2.setDirection(DcMotor.Direction.FORWARD);
+                    motor2.setPower(-finalPower3);
+                    sleep(10);
+                    motor2.setPower(0);
+                    motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
                 }
             }
+        }
 
 
 
@@ -269,52 +322,52 @@ public class test extends LinearOpMode {
 
             }*/
 
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-            leftPower = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower = Range.clip(drive - turn, -1.0, 1.0) ;
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+        double drive = -gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+        leftPower = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower = Range.clip(drive - turn, -1.0, 1.0) ;
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
 
-            class AirplaneLaunch {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.y) {
-                        servo2.setPosition(servo.getPosition() - 1);
-                    }
+        class AirplaneLaunch {
+            void execute() {
+                while (opModeIsActive() && gamepad1.y) {
+                    servo2.setPosition(servo.getPosition() - 1);
                 }
             }
+        }
 
-            class WristUp {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.dpad_up) {
-                        servo1.setPosition(servo.getPosition() + 1);
-                    }
+        class WristUp {
+            void execute() {
+                while (opModeIsActive() && gamepad1.dpad_up) {
+                    servo1.setPosition(servo.getPosition() + 1);
                 }
             }
+        }
 
-            class WristDown {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.dpad_down) {
-                        servo1.setPosition(servo.getPosition() - 1);
-                    }
+        class WristDown {
+            void execute() {
+                while (opModeIsActive() && gamepad1.dpad_down) {
+                    servo1.setPosition(servo.getPosition() - 1);
                 }
             }
+        }
 
-            class Flex {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.a) {
-                        servo.setPosition(servo.getPosition() + 1);
-                    }
+        class Flex {
+            void execute() {
+                while (opModeIsActive() && gamepad1.a) {
+                    servo.setPosition(servo.getPosition() + 1);
                 }
             }
+        }
 
-            class Extend {
-                void execute() {
-                    while (opModeIsActive() && gamepad1.b) {
-                        servo.setPosition(servo.getPosition() - 1);
-                    }
+        class Extend {
+            void execute() {
+                while (opModeIsActive() && gamepad1.b) {
+                    servo.setPosition(servo.getPosition() - 1);
                 }
             }
+        }
 
 
 
@@ -354,8 +407,5 @@ public class test extends LinearOpMode {
 
 
 
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
-        }}}
+    }
+}
