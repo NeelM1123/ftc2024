e3586376172e11e90527501c1d9d0c191e11d340
shashi <shashi.research@gmail.com>
2024-01-19 19:58:58 -0600

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name="Linear OpMode Test", group="Linear OpMode")
+public class DriveTeleOp extends LinearOpMode {
+    @TeleOp(name="Linear OpMode Test", group="Linear OpMode")
+    public class test extends  {
+}
